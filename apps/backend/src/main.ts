fb97318edb7f8054d1400c4e3cbbbd049189b388
Andrey Elyan <a.elyan.s@gmail.com>
2026-03-25 22:18:31 -0300

    feat: enhance Fastify adapter configuration with connection handling and shutdown hooks

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,8 +13,10 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({ forceCloseConnections: true }),
   );
+
+  app.enableShutdownHooks();
 
   app.enableCors({
     origin: "http://localhost:3001",
